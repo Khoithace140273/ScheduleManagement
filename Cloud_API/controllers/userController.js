18b8379cb7dbcc323b02644809f83762aef05d04
nhieuvace140337 <57791852+nhieuvace140337@users.noreply.github.com>
2021-06-30 09:28:47 +0700

    api folder sap xep function

--- Cloud_API/controllers/userController.js
+++ Cloud_API/controllers/userController.js
@@ -3,71 +3,94 @@
 const firebase = require('../db');
 const User = require('../models/userAcc');
 const firestore = firebase.firestore();
+const usersArray=[];
+
+//call function
+const updateUser = fcUpdateUser();
+const getAllUsers = fcGetAllUser();
+const addUser = fcAddUser(); 
+const getUserById =fcGetUserById();
 
 
-const addUser = async (req, res, next) => {
-    try {
-        const data = req.body;
-        await firestore.collection("User").doc().set(data);
-        res.send('Record saved successfuly');
-    } catch (error) {
-        res.status(400).send(error.message);
-    }
-}
 
-const getAllUsers = async (req, res, next) => {
-    console.log("check here");
-    try {
-        const users = await firestore.collection("User");
-        const data = await users.get();
-        const usersArray = [];
-        if(data.empty) {
-            res.status(404).send('No user record found');
-        }else {
-            data.forEach(doc => {
-                const user = new User(
-                    doc.id,
-                    doc.data().Username,
-                    doc.data().Password,
-                    doc.data().Uidfacebook,
-                    doc.data().Uidgoogle
-                );
-                usersArray.push(user);
-            });
-            res.send(usersArray);
+//function
+function fcGetUserById(){
+  const fcGetUserById  = async (req, res, next) => {
+        try {
+            const id = req.params.id;
+            const student = await firestore.collection('User').doc(id);
+            const data = await student.get();
+            if(!data.exists) {
+                res.status(404).send('User with the given ID not found');
+            }else {
+                res.send(data.data());
+            }
+        } catch (error) {
+            res.status(400).send(error.message);
         }
-    } catch (error) {
-        res.status(400).send(error.message);
     }
+    return fcGetUserById;
 }
 
-const getUserById = async (req, res, next) => {
-    try {
-        const id = req.params.id;
-        const student = await firestore.collection('User').doc(id);
-        const data = await student.get();
-        if(!data.exists) {
-            res.status(404).send('User with the given ID not found');
-        }else {
-            res.send(data.data());
+function fcAddUser(){
+
+  const fcAddUser  = async (req, res, next) => {
+       var b = usersArray.length;
+       console.log(b);
+       var a = parseInt(usersArray[usersArray.length - 1].id)+1;
+       try {
+           const data = req.body;
+           await firestore.collection("User").doc(a.toString()).set(data);
+           res.send('Record saved successfuly');
+       } catch (error) {
+           res.status(400).send(error.message);
+       }
+   }
+   return fcAddUser;
+}
+
+function fcGetAllUser (){
+    const fcArrAllUser =  async (req, res, next) => {
+        try {
+            const users = await firestore.collection("User");
+            const data = await users.get();
+            if(data.empty) {
+                res.status(404).send('No user record found');
+            }else {
+                data.forEach(doc => {
+                    const user = new User(
+                        doc.id,
+                        doc.data().Username,
+                        doc.data().Password,
+                        doc.data().Uidfacebook,
+                        doc.data().Uidgoogle
+                    );
+                    usersArray.push(user);
+                });
+                res.send(usersArray);
+            }
+        } catch (error) {
+            res.status(400).send(error.message);
         }
-    } catch (error) {
-        res.status(400).send(error.message);
+        console.log("check here");
     }
+    return fcArrAllUser;
 }
 
-const updateUser = async (req, res, next) => {
-    try {
-        const id = req.params.id;
-        const data = req.body;
-        const user =  await firestore.collection('User').doc(id);
-        await user.update(data);
-        res.send('User record updated successfuly');        
-    } catch (error) {
-        res.status(400).send(error.message);
+function fcUpdateUser(){
+ const fcupdate = async (req, res, next) => {
+        try {
+            const id = req.params.id;
+            const data = req.body;
+            const user =  await firestore.collection('User').doc(id);
+            await user.update(data);
+            res.send('User record updated successfuly');        
+        } catch (error) {
+            res.status(400).send(error.message);
+        }
     }
+    return fcupdate;
 }
-
 
 module.exports = {
     addUser,
