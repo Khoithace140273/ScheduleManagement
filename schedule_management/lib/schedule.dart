2e39e015820b2eccfe8c6055def0f1b4f350e9ff
haophu2000 <haophu275200@gmail.com>
2021-07-01 20:46:51 +0700

    thêm trang xem, tạo xóa tasks

---
+++ schedule_management/lib/schedule.dart
@@ -0,0 +1,383 @@
+import 'package:flutter/material.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+import 'bodyLogin.dart';
+import 'addNote.dart';
+import 'login.dart';
+import 'Models/TaskPages.dart';
+Widget tasksHeader() {
+  return Padding(
+    padding: const EdgeInsets.only(
+      top: 10,
+      left: 2.5,
+      right: 2.5,
+    ),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "My Tasks",
+          style: TextStyle(
+            color: Colors.blueAccent,
+            fontSize: 25.00,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        Divider(
+          color: Colors.blueAccent,
+          thickness: 2.5,
+        ),
+      ],
+    ),
+  );
+}
+
+class TaskPages extends StatefulWidget {
+  TaskPages({required Key key}) : super(key: key);
+
+  @override
+  _TaskPageState createState() => _TaskPageState();
+}
+class _TaskPageState extends State<TaskPages> {
+  var _formKey = GlobalKey<FormState>();
+  @override
+  void initState() {
+    super.initState();
+    tasksDescriptionMaxLenth =
+        tasksDescriptionMaxLines * tasksDescriptionMaxLines;
+  }
+
+  @override
+  void dispose() {
+    taskDescriptionController.dispose();
+    taskHeadingController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: tasksHeader(),
+      ),
+      body: taskHeading.length > 0
+          ? buildTasks()
+          : Center(child: Text("Add Tasks...")),
+      floatingActionButton: FloatingActionButton(
+        mini: false,
+        backgroundColor: Colors.blueAccent,
+        onPressed: () {
+          _settingModalBottomSheet(context);
+        },
+        child: Icon(Icons.create),
+      ),
+    );
+  }
+
+  Widget buildTasks() {
+    return Padding(
+      padding: const EdgeInsets.only(
+        top: 10,
+        left: 10,
+        right: 10,
+      ),
+      child: new ListView.builder(
+        itemCount: taskHeading.length,
+        itemBuilder: (context, int index) {
+          return Padding(
+            padding: const EdgeInsets.only(bottom: 5.5),
+            child: new Dismissible(
+              key: UniqueKey(),
+              direction: DismissDirection.horizontal,
+              onDismissed: (direction) {
+                setState(() {
+                  deletedTaskHeading = taskHeading[index];
+                  deletedTaskDescription = taskDescription[index];
+                  taskHeading.removeAt(index);
+                  taskDescription.removeAt(index);
+                  Scaffold.of(context).showSnackBar(
+                    new SnackBar(
+                      backgroundColor: Colors.purple,
+                      content: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          new Text(
+                            "Task Deleted",
+                            style: TextStyle(),
+                          ),
+                          deletedTaskHeading != ""
+                              ? GestureDetector(
+                            onTap: () {
+                              print("undo");
+                              setState(() {
+                                if (deletedTaskHeading != "") {
+                                  taskHeading.add(deletedTaskHeading);
+                                  taskDescription
+                                      .add(deletedTaskDescription);
+                                }
+                                deletedTaskHeading = "";
+                                deletedTaskDescription = "";
+                              });
+                            },
+                            child: new Text(
+                              "Undo",
+                              style: TextStyle(),
+                            ),
+                          )
+                              : SizedBox(),
+                        ],
+                      ),
+                    ),
+                  );
+                });
+              },
+              background: ClipRRect(
+                borderRadius: BorderRadius.circular(5.5),
+                child: Container(
+                  color: Colors.green,
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.delete,
+                            color: Colors.white,
+                          ),
+                          Text(
+                            "Delete",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              secondaryBackground: ClipRRect(
+                borderRadius: BorderRadius.circular(5.5),
+                child: Container(
+                  color: Colors.red,
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 10),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.delete,
+                            color: Colors.white,
+                          ),
+                          Text(
+                            "Delete",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              child: taskList(index),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget taskList(int index) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(5.5),
+      child: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          // color: taskColor[(index % taskColor.length).floor()],
+          borderRadius: BorderRadius.circular(5.5),
+        ),
+        height: 100,
+        child: Center(
+          child: Row(
+            children: [
+              new Container(
+                // color:
+                // taskMarginColor[(index % taskMarginColor.length).floor()],
+                width: 3.5,
+                height: double.infinity,
+              ),
+              Flexible(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 10, right: 10, top: 10),
+                  child: new Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Flexible(
+                        child: Text(
+                          taskHeading[index],
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                            fontSize: 20.00,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 2.5,
+                      ),
+                      Flexible(
+                        child: Container(
+                          height: double.infinity,
+                          child: AutoSizeText(
+                            "${(taskDescription[index])}",
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              fontSize: 15.00,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _settingModalBottomSheet(context) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      elevation: 50,
+      isDismissible: true,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(30),
+          topLeft: Radius.circular(30),
+        ),
+      ),
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      builder: (BuildContext bc) {
+        return Padding(
+          padding: EdgeInsets.only(left: 25, right: 25),
+          child: Form(
+            key: _formKey,
+            child: SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: (MediaQuery.of(context).size.height),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.only(bottom: 250, top: 50),
+                  child: new Column(
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "New Note",
+                            style: TextStyle(
+                              fontSize: 20.00,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+
+                                setState(() {
+                                  taskHeading.add(taskHeadingController.text);
+                                  taskDescription
+                                      .add(taskDescriptionController.text);
+                                  taskHeadingController.clear();
+                                  taskDescriptionController.clear();
+                                });
+                                Navigator.pop(context);
+
+
+                              print(taskHeadingController.text);
+                            },
+                            child: Container(
+                              child: Row(
+                                children: [
+                                  Text(
+                                    "Save",
+                                    style: TextStyle(
+                                      fontSize: 20.00,
+                                      color: Colors.blue,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Divider(
+                        color: Colors.blueAccent,
+                        thickness: 2.5,
+                      ),
+                      TextFormField(
+                        maxLength: tasksHeaderMaxLenth,
+                        controller: taskHeadingController,
+                        decoration: InputDecoration(
+                          hintText: "Task Heading",
+                          hintStyle: TextStyle(
+                            fontSize: 15.00,
+                            color: Colors.grey,
+                            fontWeight: FontWeight.w500,
+                          ),
+                          prefixIcon: Icon(Icons.text_fields),
+                        ),
+
+                        onFieldSubmitted: (String value) {
+                          FocusScope.of(context)
+                              .requestFocus(textSecondFocusNode);
+                        },
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 15),
+                        child: Container(
+                          margin: EdgeInsets.all(1),
+                          height: 5 * 24.0,
+                          child: TextFormField(
+                            focusNode: textSecondFocusNode,
+                            maxLines: tasksDescriptionMaxLines,
+                            maxLength: tasksDescriptionMaxLenth,
+                            controller: taskDescriptionController,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              hintText: 'Description',
+                              hintStyle: TextStyle(
+                                fontSize: 15.00,
+                                color: Colors.grey,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
